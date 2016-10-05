6f755b76b25ed3dc992cb5c1c7c33c4c65ef6b70
1-025 <1-025@1-025-PC>
2016-10-05 20:34:26 +0900

    K

--- korean/src/LeeSense/sleepy.java
+++ korean/src/LeeSense/sleepy.java
@@ -1,5 +1,16 @@
 package LeeSense;
 
 public class sleepy {
+	private String nado;
 
+	public String getNado() {
+		return nado;
+	}
+
+	public void setNado(String nado) {
+		this.nado = nado;
+	}
+	
+	
+	
 }
