c928636488b2532a5e475224511ddc783b299e1c
daheen2 <daheen2@gmail.com>
2016-10-21 22:39:28 +0900

    disconnect by K

---
+++ VideoCall/WebContent/script/callee.js
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+ 'use strict';
+ 
+    var connectChannelIdInput = document.querySelector('#connectChannelId');
+    var connectChannelButton = document.querySelector('#connectChannel');
+    var connectChannelValue = document.getElementById('#channelId_callee');
+    var appCallee;
+    var channelId = '';
+    var pid = '';
+
+    appCallee = new PlayRTC({
+      projectKey: '60ba608a-e228-4530-8711-fa38004719c1',
+      localMediaTarget: 'calleeLocalVideo',
+      remoteMediaTarget: 'calleeRemoteVideo',
+      ring: true
+    });
+
+    appCallee.on('ring', function(pid, uid) {
+    	this.pid = pid;
+    	alert('pid는 :' + pid);
+      if (window.confirm('callee : Would you like to get a call?')) {
+    	  alert('callee: channel:'+channelId);
+        appCallee.accept(pid);
+      } else {
+        appCallee.reject(pid);
+      }
+    });
+
+    connectChannelButton.addEventListener('click', function(event) {
+      channelId = connectChannelIdInput.value;
+      alert('callee:'+channelId);
+      
+     // connectChannelValue.innerHTML = channelId;
+
+      event.preventDefault();
+      if (!channelId) {
+        return;
+      }
+      appCallee.connectChannel(channelId);
+      alert('callee : connectChannel' + channelId);
+    }, false);
+    
+    $('#disconnectChannel_callee').on('click', function(event){
+    	event.preventDefault();
+    	if(window.confirm('통화를 종료하시겠습니까?')){
+    		appCallee.disconnectChannel(pid);
+    		// 상대방한테 통화가 종료되었습니다 alert 띄울라면?
+    	} else {
+    	}
+    });
